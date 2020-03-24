a75b8fb1d96f35e7392ed00fbea0111a04d1982c
GitRAK07 <43309791+GitRAK07@users.noreply.github.com>
2020-03-24 16:36:32 +0530

    Update jenkinsfile.tf

--- jenkinsfile.tf
+++ jenkinsfile.tf
@@ -1,10 +1,10 @@
 provider "aws" {
   region     = "ap-south-1"
-  access_key = "access_key"
-  secret_key = "secret_key"
+  access_key = "access_keys"
+  secret_key = "secret_keys"
 } 
 
 resource "aws_instance" "instance1" {
-    ami = "ami"
+    ami = "ami-id"
     instance_type = "t2.micro"
 }
